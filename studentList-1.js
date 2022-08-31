4152f28ce283f7dc3d8df67afbd46c99108b2474
jeonhyoungmin <jhmin7509@gmail.com>
2022-08-31 11:48:38 +0900

    생성자 함수에 없는 매개변수 자리를 생성자 함수 외부에서 새롭게 생성할 수 있다.

---
+++ studentList-1.js
@@ -0,0 +1,21 @@
+const members = [];
+
+const studentList = ["강","김","류","마","박","박","송","양","원","유","이","이","이","정","정","정","지","한","황","김","김","전","정","이","최"
+];
+
+function Kdt(id, name){
+  if(typeof (id) === 'number'
+      && typeof (name) === 'string'){
+        this.id = id;
+        this.name = name;
+      }
+}
+
+for(let i = 0; i<studentList.length; i++){
+  members.push(new Kdt(i+1, studentList[i]))
+  if(studentList[i] === "전"){
+    members[i].address = '중구'
+  }
+}
+
+console.table(members)
